a67695ae4d4e9c825fb470a4db2df435cba9331e
Ryan Dulac <analyticrye@gmail.com>
2019-07-17 01:50:16 -0400

    added addItem and removeItem functionality to checkout-item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -13,23 +13,26 @@ import "./checkout.style.scss";
 const Checkout = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
-      <div className="header-block" />
-      <span>Product</span>
-      <div className="header-block" />
-      <span>Description</span>
-      <div className="header-block" />
-      <span>Quanity</span>
-      <div className="header-block" />
-      <span>Price</span>
-      <div className="header-block" />
-      <span>Remove</span>
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
     </div>
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <div>TOTAL: ${total}</div>
-    </div>
+    <div className="total">TOTAL: ${total}</div>
   </div>
 );
 
